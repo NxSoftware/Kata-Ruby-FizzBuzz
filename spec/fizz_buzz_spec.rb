17092616146f93cba49123bcabf055dfe4ada038
Steve Wilford <steve@stevewilford.co.uk>
2017-01-04 20:21:49 +0000

    Refactor tests

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,15 +1,17 @@
 RSpec.describe FizzBuzz, '#value' do
+  before(:example) do
+    @fizzBuzz = FizzBuzz.new
+  end
+  
   context 'given 1' do
     it 'returns 1' do
-      fb = FizzBuzz.new
-      expect(fb.value 1).to eq 1
+      expect(@fizzBuzz.value 1).to eq 1
     end
   end
   
   context 'given 2' do
     it 'returns 2' do
-      fb = FizzBuzz.new
-      expect(fb.value 2).to eq 2
+      expect(@fizzBuzz.value 2).to eq 2
     end
   end
 end
